da0742ead0037af0a01f2616d3e59b8fd00f31d5
chase hermanson <chase.hermanson@yahoo.com>
2018-08-24 15:07:05 -0500

    Add files via upload

---
+++ FizzBuzz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main (void)
+{
+int b;
+for (b = 1; b <= 100; b++)
+{
+if(!(b % 15))
+printf("FizzBuzz");
+else if(!(b % 3))
+printf("Fizz");
+else if(!(b % 5))
+printf("Buzz");
+else
+printf("%d", b);
+printf("\n");
+}
+return 0;
+}
